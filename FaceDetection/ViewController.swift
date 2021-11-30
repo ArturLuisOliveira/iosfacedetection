684a8ed2459eb727fd5f2f240e01af951003555a
artur <arturluisoliveira@gmail.com>
2021-11-30 15:02:20 -0300

    camera access

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -6,15 +6,87 @@
 //
 
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
-
+class ViewController:  UIViewController, AVCapturePhotoCaptureDelegate {
+    private let photoOutput = AVCapturePhotoOutput()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        openCamera()
     }
-
-
+    
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        
+    }
+    
+    private func openCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: // the user has already authorized to access the camera.
+            self.setupCaptureSession()
+            
+        case .notDetermined: // the user has not yet asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { (granted) in
+                if granted { // if user has granted to access the camera.
+                    print("the user has granted to access the camera")
+                    DispatchQueue.main.async {
+                        self.setupCaptureSession()
+                    }
+                } else {
+                    print("the user has not granted to access the camera")
+                    self.handleDismiss()
+                }
+            }
+            
+        case .denied:
+            print("the user has denied previously to access the camera.")
+            self.handleDismiss()
+            
+        case .restricted:
+            print("the user can't give camera access due to some restriction.")
+            self.handleDismiss()
+            
+        default:
+            print("something has wrong due to we can't access the camera.")
+            self.handleDismiss()
+        }
+    }
+    
+    private func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        if let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice)
+                if captureSession.canAddInput(input) {
+                    captureSession.addInput(input)
+                }
+            } catch let error {
+                print("Failed to set input device with error: \(error)")
+            }
+            
+            if captureSession.canAddOutput(photoOutput) {
+                captureSession.addOutput(photoOutput)
+            }
+            
+            let cameraLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            cameraLayer.frame = self.view.frame
+            cameraLayer.videoGravity = .resizeAspectFill
+            self.view.layer.addSublayer(cameraLayer)
+            
+            captureSession.startRunning()
+            self.setupUI()
+        }
+    }
+    
+    private func handleDismiss() {
+        DispatchQueue.main.async {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
 }
 
